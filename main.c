9cfd9a33025c9817f808add642442c17aa5597c9
eignnx <eignnx@gmail.com>
2018-01-13 17:24:16 -0500

    Impl pr_menu fn

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>      // open
-#include <stdlib.h>
+#include <ctype.h>      // isdigit
+
 #include "dog_entry.h"
 
 #define INP_BUF_SZ 100
@@ -22,29 +24,31 @@ off_t find_dog(int);         // returns offset into the file
 int main()
 {
     char input[INP_BUF_SZ];
-    char filebuf[RD_BUF_SZ];
-    int bytes_read;
+    
+    
+    // char filebuf[RD_BUF_SZ];
+    // int bytes_read;
 
-    int fd = open("database.dog", O_RDONLY);
+    // int fd = open("database.dog", O_RDWR);
 
-    if (fd == -1) {
-        perror("Could not find database.dog");
-        exit(-1);
-    }
+    // if (fd == -1) {
+    //     perror("Could not find database.dog");
+    //     exit(-1);
+    // }
 
-    while ((bytes_read = read(fd, filebuf, RD_BUF_SZ)) != 0) {
-        for (int i = 0; i < bytes_read; i++)
-            putchar(filebuf[i]);
-    }
+    // while ((bytes_read = read(fd, filebuf, RD_BUF_SZ)) != 0)
+    //     for (int i = 0; i < bytes_read; i++)
+    //         putchar(filebuf[i]);
 
     
     prompt("Enter a command.");
     while (fgets(input, INP_BUF_SZ, stdin) != NULL) {
         strip_nl(input);
         
-        printf(">> '%s'\n", input);
+        //printf(">> '%s'\n", input);
         
-        prompt(NULL);
+        printf(">> '%c'\n", pr_menu());
+        //prompt(NULL);
     }
 
     exit(0);
@@ -63,4 +67,22 @@ void strip_nl(char *str)
     size_t len = strlen(str);
     if (str[len - 1] == '\n')
         str[len - 1] = '\0';
+}
+
+unsigned char pr_menu()
+{
+    printf("ENTER%5d %s\n", 1, "to add,");
+    printf("%10d %s\n",     2, "to change,");
+    printf("%10d %s\n",     3, "to delete,");
+    printf("%10d %s\n",     4, "to view,");
+    printf("%10d %s\n",     5, "to search,");
+    printf("%10d %s\n",     6, "to exit");
+    printf("ENTER: ");
+
+    int c = getchar();
+    int i = c - '0';
+    if (isdigit(c) && i <= 6 && i >= 1)
+        return (unsigned char) c;
+    else
+        return '\0';
 }
